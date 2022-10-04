cc6afdc7995cda2a6534ffa09bc837750f0833ab
LimitedMouse <112146251+LimitedMouse@users.noreply.github.com>
2022-10-04 12:42:20 +0800

    Add files via upload

---
+++ 1helloworld.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+/*这里是多行注释，下边是main函数，是程序的入口，每个程序都有且只有一个*/
+int main()
+{
+	//这是一条单行注释，7行代码输出hello world
+	cout << "hello world!" << endl;
+	system("pause");
+
+	return 0;
+}
